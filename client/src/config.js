e6c17ad9535044e80d63d25cadb25fdd24c626ff
Mauro Parafati <mparafati@fi.uba.ar>
2023-06-16 10:27:20 +0200

    Update example

--- client/src/config.js
+++ client/src/config.js
@@ -1,15 +1,13 @@
-const host = process.env.REACT_APP_API_HOST || 'localhost';
-const port = process.env.REACT_APP_API_PORT || 3000;
+const host = process.env.REACT_APP_API_HOST || 'localhost:3000';
 const secureProtocols =
   (process.env.REACT_APP_SECURE_PROTOCOLS || 'true').toLowerCase() === 'true';
 
 const config = {
   api: {
     host,
-    port,
     route: process.env.REACT_APP_API_ROUTE || 'api',
     secureProtocols,
-    baseUrl: `${secureProtocols ? 'https' : 'http'}://${host}:${port}`,
+    baseUrl: `${secureProtocols ? 'https' : 'http'}://${host}`,
   },
 };
 
