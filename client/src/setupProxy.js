087fcdfebaa649ac9f2b89447af1200163185b22
Mauro Parafati <mauro.parafati@datadoghq.com>
2024-04-18 10:57:45 +0200

    Bump http-proxy-middleware from v2 to v3

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const port = process.env.REACT_APP_DEV_SERVER_PORT || 3000;
+const paths = ['/api', '/logout', '/login', '/user', '/callback'];
 
 module.exports = function (app) {
-  app.use(
-    ['/api', '/logout', '/login', '/user', '/callback'],
-    createProxyMiddleware({
-      target: `http://localhost:${port}`,
-      changeOrigin: true,
-      xfwd: true,
-    }),
-  );
+  paths.forEach((path) => {
+    const target = `http://localhost:${port}/${path}`;
+    app.use(
+      path,
+      createProxyMiddleware({
+        target,
+        changeOrigin: true,
+        xfwd: true,
+      }),
+    );
+  });
 };
